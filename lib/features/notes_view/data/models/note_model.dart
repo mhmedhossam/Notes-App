ad73c0e47cb8dfa990122edd9be736c05c1c14a9
mhmedhossam <momohossam11.mh@gmail.com>
2025-11-18 20:04:37 +0200

    create type adabter for note app

--- lib/features/notes_view/data/models/note_model.dart
+++ lib/features/notes_view/data/models/note_model.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+part 'note_model.g.dart';
 
-class NoteModel {
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subTitle;
+  @HiveField(4)
   final VoidCallback onTap;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
   NoteModel({
     required this.title,
